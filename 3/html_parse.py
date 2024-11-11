0ed3d357690ce905dc6a8044c1416cdae630a6ae
vitosgeen <vitosgeen@yandex.ru>
2024-11-11 18:37:52 +0200

    fix data to data_tree for parse_html_xpath

--- 3/html_parse.py
+++ 3/html_parse.py
@@ -10,23 +10,23 @@ def parse_html_xpath(url):
     
     # parse html
     try:
-        data = etree.HTML(html)
+        data_tree = etree.HTML(html)
     except Exception as e:
         print("Error etree.HTML")
         return None
     
     # find input field search
-    search_element_what = data.xpath('//input[@id="text-input-what"]')
+    search_element_what = data_tree.xpath('//input[@id="text-input-what"]')
     if not search_element_what:
         print("search_element_what not found")
 
     # find input field search by region
-    search_element_where = data.xpath('//input[@id="text-input-where"]')
+    search_element_where = data_tree.xpath('//input[@id="text-input-where"]')
     if not search_element_where:
         print("search_element_where not found")
 
     # find search button form#jobsearch button[type="submit"]
-    search_button = data.xpath('//form[@id="jobsearch"]//button[@type="submit"]')
+    search_button = data_tree.xpath('//form[@id="jobsearch"]//button[@type="submit"]')
     if not search_button:
         print("search_button not found")
 
