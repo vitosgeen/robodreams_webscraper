972ec4b2e9f45b616ab3012d8cc8678b2df44c47
vitosgeen <vitosgeen@yandex.ru>
2024-11-11 18:36:08 +0200

    Refactor parse_html_xpath to use etree.HTML and add error handling for missing elements

--- 3/html_parse.py
+++ 3/html_parse.py
@@ -4,25 +4,31 @@ import xml.etree.ElementTree as ET
 from lxml import etree
 
 def parse_html_xpath(url):
-    page = download.download_file(url)
-    if page is None:
-        return None
+    html = download.download_file(url)
+    if html is None:
+        return None    
     
     # parse html
     try:
-        data = ET.parse(page)
+        data = etree.HTML(html)
     except Exception as e:
-        print("Error etree.parse")
+        print("Error etree.HTML")
         return None
     
     # find input field search
     search_element_what = data.xpath('//input[@id="text-input-what"]')
+    if not search_element_what:
+        print("search_element_what not found")
 
     # find input field search by region
     search_element_where = data.xpath('//input[@id="text-input-where"]')
+    if not search_element_where:
+        print("search_element_where not found")
 
     # find search button form#jobsearch button[type="submit"]
     search_button = data.xpath('//form[@id="jobsearch"]//button[@type="submit"]')
+    if not search_button:
+        print("search_button not found")
 
     print(search_element_what)
     print(search_element_where)
